259185d1d5ed3c18317a1c1745dfc16385a9930e
Jugger788 <110557298+Jugger788@users.noreply.github.com>
2022-08-03 13:12:37 -0500

    Update filter.js

--- filter.js
+++ filter.js
@@ -33,12 +33,9 @@ function getUniqueValuesFromColumn() {
             }
         });
 
-        
     });
 
-    for(i in unique_col_values_dict) {
-        alert("Column index : " + i + " has Unique values : \n" + unique_col_values_dict[i]);
-    }
+    
 
     updateSelectOptions(unique_col_values_dict)
 
